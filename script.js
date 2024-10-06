0556054bc443c288a11e54028dd48d476b7c0623
arjavlamsal <lamsalarjav@gmail.com>
2024-10-06 09:25:55 -0500

    footer, test_img, contact added

--- script.js
+++ script.js
@@ -331,3 +331,16 @@ document.getElementById("download-img").addEventListener("click", function () {
 	// Trigger the download
 	downloadLink.click();
 });
+
+
+
+// Contact 
+const msg = document.querySelector(".msg");
+const sumbitbtn = document.getElementById("submitbtn");
+
+msg.addEventListener("focus", submsg);
+
+function submsg() {
+  sumbitbtn.style.display = "block";
+  sumbitbtn.style.animation = "slide 0.5s ease-in-out";
+}
